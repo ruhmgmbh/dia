c0e7b5ed8d06405b3415276d6c753ef97146f536
patrick <76250863+patricksdev@users.noreply.github.com>
2025-06-11 13:07:47 +0200

    now...

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -16,9 +16,9 @@ export default async function Page() {
   return (
     <>
       <div className="relative bg-dia-yellow">
-        <div className="relative container">
+        <div className="relative">
 
-          <header className="py-6 bg-dia-yellow pt-30 xl:pt-32">
+          <header className="py-6 bg-dia-yellow pt-30 xl:pt-32 container">
             <h1 className="text-h1 mb-5 lg:mb-12">
               Wir sind eine Agentur für interaktive Marken, Plattformen und
               Erlebnisse.
